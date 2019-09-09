84409da5e308cee39730a88e84af7de8537e9b64
Sudharshana VP <vpsudharshana@gmail.com>
2019-09-10 02:21:00 +0530

    Added more content and Styling
    Added proper hyperlink animation.
    Made each service grid clickable and hyperlink lights up.
    Made the navbar using flex.
    Added image to the home page
    Removed Our Work section on home page

---
+++ main.js
@@ -0,0 +1,8 @@
+$(".services").click(function() {
+    window.location = $(this).find("a").attr("href"); 
+    return false;
+  });
+
+$(".services").mouseover(function () {
+    $("p").css('cursor','pointer');    
+});
